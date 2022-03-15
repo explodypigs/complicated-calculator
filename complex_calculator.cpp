4518284ef47d337171336f3c8e69ff0791e7dd95
explodypigs <71389849+explodypigs@users.noreply.github.com>
2022-03-15 15:37:10 -0500

    Added more to calculations

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -1,36 +1,33 @@
 #include<iostream>
-#include<stdio.h>
 #include<string>
 #include<vector>
 using namespace std;
-string* find_sub_operations(string,bool);
+string* find_sub_operations(string);
 string solve_eq(string,int);
+string format_equation(string,int,int,string);
 int main(){
     string opera;
-    cout<<"type in your problem(no spaces or letters, also stick with conventional formats)"<<endl;
+    cout<<"use conventional formats"<<endl;
+    cout<<"type in your problem"<<endl;
     cin>>opera;
-    string* sube=new string[3];
-    sube = find_sub_operations(opera);
-    cout<<sube[2];
+    solve_eq(opera,1);
+    string* sube = find_sub_operations(opera);
+    cout<<sube;
     return 0;
 }
-string* find_sub_operations(string oper, bool par_found=false){
-    if(par_found!=true){
+string* find_sub_operations(string oper){
         if(oper.find('(')!=-1){
             bool pcheck=true;
             int p1=oper.find_last_of('(');
             int p2=oper.find(')',p1);
-            auto sp1 = std::to_string(p1);
-        auto sp2 = std::to_string(p2);
+            string sp1 = std::to_string(p1);
+        string sp2 = std::to_string(p2);
         string* subeq = new string[3];
         subeq[0]=sp1;
         subeq[1]=sp2;
         subeq[2]=oper.substr(p1+1,p2-p1-1);
         return subeq;
         }
-        cout<<endl<<"no parenthesis"<<endl;
-        return 0;
-    }
     vector <int> divpos;
     vector <int> mulpos;
     vector <int> addpos;
@@ -73,28 +70,74 @@ string* find_sub_operations(string oper, bool par_found=false){
     }
     return 0;
 }
-string solve_eq(string oper, int pos){
-    bool check=true;
+string solve_eq(string operation, int pos){
     int startpos;
-    int endpos;    
-    while(check){
-        for(int i=2;i<oper.length()-pos;i++){
-
-        }
-        for(int i=2;i<oper.length()-pos;i++){
-            
+    int endpos;
+    //fix the following
+    for(int i=1;i<operation.length()-pos;i++){
+        if(i<operation.length()){
+            switch (operation.at(pos-i)){
+                case '1':break;
+                case '2':break;
+                case '3':break;
+                case '4':break;
+                case '5':break;
+                case '6':break;
+                case '7':break;
+                case '8':break;
+                case '9':break;
+                case '0':break;
+                default:startpos=pos-i;i=operation.length()+1;break;
+            }
         }
     }
-    if (oper.at(pos)=='+'){
-
+    for(int i=1;i<operation.length()-pos;i++){
+        if(i<operation.length()){
+            switch (operation.at(pos+i)){
+                case '1':break;
+                case '2':break;
+                case '3':break;
+                case '4':break;
+                case '5':break;
+                case '6':break;
+                case '7':break;
+                case '8':break;
+                case '9':break;
+                case '0':break;
+                default:endpos=pos-i;i=operation.length()+1;break;
+            }
+        }
     }
-    else if (oper.at(pos)=='-'){
+    cout<<startpos<<operation.substr(startpos,pos+1-startpos)<<endl;
+    int x = stoi(operation.substr(startpos,pos+1-startpos));
+    int y = stoi(operation.substr(pos+1,endpos-pos));
+    int ans;
+    cout<<x<<endl;
+    if (operation.at(pos)=='+'){
         
     }
-    else if (oper.at(pos)=='/'){
+    else if (operation.at(pos)=='-'){
         
     }
-    else if (oper.at(pos)=='*'){
+    else if (operation.at(pos)=='/'){
+        
+    }
+    else if (operation.at(pos)=='*'){
         
     }
 }
+string format_eq(string txt,int start, int end,string insert){
+        switch (txt.at(start)){
+            case '1':break;
+            case '2':break;
+            case '3':break;
+            case '4':break;
+            case '5':break;
+            case '6':break;
+            case '7':break;
+            case '8':break;
+            case '9':break;
+            case '0':break;
+            default:break;
+        }
+    }
