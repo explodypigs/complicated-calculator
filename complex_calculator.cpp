7e2e4384c4351f15cdcc74410c998eadcc478fe1
explodypigs <71389849+explodypigs@users.noreply.github.com>
2022-03-15 20:57:51 -0500

    still alot of problems

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -2,53 +2,63 @@
 #include<string>
 #include<vector>
 using namespace std;
-string* find_sub_operations(string);
-string solve_eq(string,int);
-string format_equation(string,int,int,string);
+string* find_par(string);
+string* solve_eq(string,int);
+string* order_eq(string);
+string format_eq(string,int,int,string);
 int main(){
+    bool check=true;
+    string* ins=new string[3];
     string opera;
     cout<<"use conventional formats"<<endl;
     cout<<"type in your problem"<<endl;
     cin>>opera;
-    solve_eq(opera,1);
-    string* sube = find_sub_operations(opera);
-    cout<<sube;
+    while(check){
+        string* info=new string[3];
+        info=find_par(opera);
+        if(info[0]!="stop"){
+            ins=order_eq(info[2]);
+            opera=format_eq(opera,stoi(info[0]),stoi(info[1]),ins[2]);
+        }
+        else{
+            check=false;
+        }
+    }
+    ins=order_eq(opera);
+    opera=format_eq(opera,0,opera.length(),ins[2]);
+    cout<<opera<<endl;
     return 0;
 }
-string* find_sub_operations(string oper){
+string* find_par(string oper){
+    string* subeq = new string[3];
         if(oper.find('(')!=-1){
             bool pcheck=true;
             int p1=oper.find_last_of('(');
             int p2=oper.find(')',p1);
             string sp1 = std::to_string(p1);
         string sp2 = std::to_string(p2);
-        string* subeq = new string[3];
         subeq[0]=sp1;
         subeq[1]=sp2;
         subeq[2]=oper.substr(p1+1,p2-p1-1);
         return subeq;
         }
-    vector <int> divpos;
-    vector <int> mulpos;
-    vector <int> addpos;
-    vector <int> subpos;
+    subeq[0]="stop";
+    return subeq;
+}
+string* order_eq(string oper){
+    vector <int> muldivpos;
+    vector <int> subaddpos;
     for(int i=0;i<5;i++){
         char operations[4]={'+','-','*','/'};
         bool cont=true;
         if(oper.find(operations[i])!=-1){
             int pos = oper.find(operations[i]);
             while(cont){
-                if(i==0){
-                    addpos.push_back(pos);
+                if(i==0 or i==1){
+                    subaddpos.push_back(pos);
                 }
-                else if(i==1){
-                    subpos.push_back(pos);
-                }
-                else if(i==2){
-                    mulpos.push_back(pos);
-                }
-                else if(i==3){
-                    divpos.push_back(pos);
+                else if(i==2 or i==3){
+                    muldivpos.push_back(pos);
                 }
                 if(oper.find(operations[i],pos+1)!=-1){
                     pos=oper.find(operations[i],pos+1);
@@ -58,25 +68,26 @@ string* find_sub_operations(string oper){
                 }
             }
         }
-        if(divpos.size()!=0 and mulpos.size()!=0){
-
-        }
-        else if(divpos.size()!=0){
-
-        }
-        else if(mulpos.size()!=0){
-
+    }
+    string* operinfo=new string[3];
+    if(muldivpos.size()!=0){
+        for(int i=0;i<muldivpos.size();i++){
+            operinfo=solve_eq(oper,muldivpos[i]);
         }
     }
-    return 0;
+    if(subaddpos.size()!=0){
+        for(int i=0;i<subaddpos.size();i++){
+            operinfo=solve_eq(oper,subaddpos[i]);
+        }
+    }
+    return operinfo;
 }
-string solve_eq(string operation, int pos){
-    int startpos;
-    int endpos;
-    //fix the following
-    for(int i=1;i<operation.length()-pos;i++){
-        if(i<operation.length()){
-            switch (operation.at(pos-i)){
+string* solve_eq(string operation, int pos){
+    int startpos=0;
+    int endpos=0;
+    int num;
+    for(int i=1;(pos-i)>=0;i++){
+            switch (operation[pos-i]){
                 case '1':break;
                 case '2':break;
                 case '3':break;
@@ -89,10 +100,16 @@ string solve_eq(string operation, int pos){
                 case '0':break;
                 default:startpos=pos-i;i=operation.length()+1;break;
             }
-        }
+            if(startpos!=0){
+                i=324343;
+            }
+            num=i;
     }
-    for(int i=1;i<operation.length()-pos;i++){
-        if(i<operation.length()){
+    if(startpos==0){
+        startpos=pos-num;
+    }
+    cout<<operation.substr(startpos,pos-startpos)<<endl;
+    for(int i=1;pos+i<operation.length();++i){
             switch (operation.at(pos+i)){
                 case '1':break;
                 case '2':break;
@@ -104,40 +121,72 @@ string solve_eq(string operation, int pos){
                 case '8':break;
                 case '9':break;
                 case '0':break;
-                default:endpos=pos-i;i=operation.length()+1;break;
-            }
+                default:endpos=pos+i;i=operation.length()+1;break;
         }
     }
-    cout<<startpos<<operation.substr(startpos,pos+1-startpos)<<endl;
-    int x = stoi(operation.substr(startpos,pos+1-startpos));
-    int y = stoi(operation.substr(pos+1,endpos-pos));
-    int ans;
-    cout<<x<<endl;
+    float x = std::stof(operation.substr(startpos,pos-startpos));
+    float y = std::stof(operation.substr(pos+1,endpos-pos));
+    cout<<"worked"<<endl;
+    double ans;
     if (operation.at(pos)=='+'){
-        
+        ans = x+y;;
     }
     else if (operation.at(pos)=='-'){
-        
+        ans=x-y;;
     }
     else if (operation.at(pos)=='/'){
-        
+        ans=x/y;
     }
     else if (operation.at(pos)=='*'){
-        
+        ans=x*y;
     }
+    string strans=to_string(ans);
+    string* operinfo=new string[3];
+    operinfo[0]=to_string(startpos+1);
+    operinfo[1]=to_string(endpos);
+    operinfo[2]=strans;
+    return operinfo;
 }
 string format_eq(string txt,int start, int end,string insert){
-        switch (txt.at(start)){
-            case '1':break;
-            case '2':break;
-            case '3':break;
-            case '4':break;
-            case '5':break;
-            case '6':break;
-            case '7':break;
-            case '8':break;
-            case '9':break;
-            case '0':break;
-            default:break;
+    bool set1=false;
+    bool set2=false;
+    cout<<start;
+    if(start-1>0){
+        switch (txt.at(start-1)){
+            case '-':break;
+            case '+':break;
+            case '/':break;
+            case '*':break;
+            case ')':break;
+            default:set1=true;
         }
     }
+    if(end-1<=txt.length()){
+        cout<<"ye";
+        switch (txt.at(end-1)){
+            case '-':break;
+            case '+':break;
+            case '/':break;
+            case '*':break;
+            case ')':break;
+            default:set2=true;
+        }
+        cout<<set2;
+    }
+    if(set1==false and set2==false){
+        txt.replace(start,end-start,insert);
+        return txt;
+    }
+    else if(set1==true and set2==true){
+        txt.replace(start,end-start,"*"+insert+"*");
+        return txt;
+    }
+    else if(set1==true){
+        txt.replace(start,end-start,"*"+insert);
+        return txt;
+    }
+    else{
+        txt.replace(start,end-start,insert+"*");
+        return txt;
+    }
+}
